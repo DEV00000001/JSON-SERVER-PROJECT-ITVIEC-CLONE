19e1b0d3fdcfc9c76f9b17a6a91643675269e2e4
DEV00000001 <vuongdachaivang@gmail.com>
2025-06-22 18:56:09 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // hoặc đường dẫn khác nếu bạn lưu db.json ở nơi khác
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
